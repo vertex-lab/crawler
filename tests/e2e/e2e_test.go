1662dfd136f741a66bfabb1406035d3d6959ec32
pippellia-btc <pippellia@gmail.com>
2025-03-19 17:39:10 +0100

    better benchmarks

--- tests/e2e/e2e_test.go
+++ tests/e2e/e2e_test.go
@@ -249,16 +249,20 @@ func BenchmarkPagerank(b *testing.B) {
 		b.Fatalf("NewRWSConnection(): benchmark failed: %v", err)
 	}
 
-	const size = 1000
-	nodeIDs := make([]uint32, size)
-	for i := 0; i < size; i++ {
-		nodeIDs[i] = uint32(i)
-	}
+	sizes := []int{100, 1000, 10000, 100000}
+	for _, size := range sizes {
+		b.Run(fmt.Sprintf("size=%d", size), func(b *testing.B) {
+			nodeIDs := make([]uint32, size)
+			for i := 0; i < size; i++ {
+				nodeIDs[i] = uint32(i)
+			}
 
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		if _, err := pagerank.Global(ctx, RWS, nodeIDs...); err != nil {
-			b.Fatalf("benchmark failed: %v", err)
-		}
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				if _, err := pagerank.Global(ctx, RWS, nodeIDs...); err != nil {
+					b.Fatalf("benchmark failed: %v", err)
+				}
+			}
+		})
 	}
 }
